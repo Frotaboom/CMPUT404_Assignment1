7f17359152f0bb60db9005ea05f28eadfc2e3189
Tomas <tomas.peschke@gmail.com>
2022-09-29 23:59:23 -0600

    Completion of server.py and addition of firefox ss

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 #  coding: utf-8 
 import socketserver
+import os
 
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
 # 
@@ -26,13 +27,163 @@ import socketserver
 
 # try: curl -v -X GET http://127.0.0.1:8080/
 
-
 class MyWebServer(socketserver.BaseRequestHandler):
+
+    baseUrl = "localhost:8080"
+
+    #sends a 200 OK response. Takes a path to a file to send back to the client
+    def response200(self, validatedPath):
+        packet = "HTTP/1.1 200 OK\r\n"
+        packet += "Server: " + self.baseUrl + "\r\n"
+        fileType = validatedPath.split('.')[len(validatedPath.split('.'))-1]
+        packet += "Content-Type: text/" + fileType + "\r\n"
+        packet += '\r\n'
+        packet += self.getFile(validatedPath)
+        self.request.sendall(bytearray(packet, 'utf-8'))
+
+    #sends a 301 response with a URI to give to the client for the place the client should go to
+    def response301(self, URI):
+        packet = "HTTP/1.1 301 Permanently Moved\r\n"
+        packet += "Server: " + self.baseUrl + "\r\n"
+        packet += "Location: " + URI + "\r\n\r\n"  
+        self.request.sendall(bytearray(packet, 'utf-8'))
+
+    #sends a 404 response to the client informing them that their requested path does not exist
+    def response404(self):
+        packet = "HTTP/1.1 404 Not Found\r\n"
+        packet += "Server: " + self.baseUrl + "\r\n\r\n"
+        self.request.sendall(bytearray(packet, 'utf-8'))
+
+    #sends a 405 response letting the client know that their method is not being handled
+    def response405(self):
+        packet = "HTTP/1.1 405 Method Not Allowed\r\n"
+        packet += "Server: " + self.baseUrl + "\r\n\r\n"
+        self.request.sendall(bytearray(packet, 'utf-8'))
+
+    #takes in a request and sends out 405s for all non-get requests and invalid requests
+    #and returns a path for further processing on valid get requests
+    def processRequest(self):
+        intake = self.data.decode('utf-8')
+        lines = intake.split('\r\n')
+
+        if len(lines) == 0:
+            #blank request - 405
+            return False
     
+        firstLine = lines[0].split(' ')
+        if len(firstLine) == 0:
+            #blank first line - 405
+            return False
+
+        typeRequest = firstLine[0]
+        if typeRequest.lower() != 'get':
+            #unhandled HTTP request - 405
+            return False
+
+        return self.percentDecode(firstLine[1])
+
+    #from stack overflow: https://stackoverflow.com/questions/16566069/url-decode-utf-8-in-python
+    #replaces all % characters with their ASCII equivalents
+    def percentDecode(self, string):
+        decodedString = ""
+        idx = 0
+        while idx < len(string):
+            if string[idx] == '%':
+                byteArray = bytearray()
+                byteArray.append(int(string[idx+1:idx+3], 16))
+                decodedString += byteArray.decode('utf-8')
+                idx += 3
+            else:
+                decodedString += string[idx]
+                idx += 1
+
+        return decodedString
+
+    #returns False for a path that does not exist
+    #returns the path to the FILE request
+    #if requested a dir, returns a path to the "index.html" file in that folder (if exists)
+    def validateFilePath(self, path):
+        if "/./" in path:
+            return False
+
+        if path in ("", "/", "/index.html"):
+            return "/index.html"
+
+        pathPoints = path.split('/')
+        pathLength = len(pathPoints)
+        if pathPoints[pathLength-1] == "":
+            pathLength -= 1
+
+        pathToReturn = ""
+        for pathCounter in range(1, pathLength):
+            nextSegment = pathPoints[pathCounter]
+            proceed = False
+            for root, dirs, files in os.walk("./www" + pathToReturn):
+                if pathCounter + 1 >= pathLength:
+                    for name in files:
+                        if name == nextSegment:
+                            return pathToReturn + "/" + nextSegment
+
+                    for name in dirs:
+                        if name == nextSegment:
+                            #last segment is a dir. Should return the index.html of this dir
+                            pathToReturn += "/" + nextSegment
+                            finalDirsChildren = os.walk("./www" + pathToReturn)
+                            for root2, finalDirs, finalFiles in finalDirsChildren:
+                                for fileName in finalFiles:
+                                    if fileName == "index.html":
+                                        return pathToReturn + "/index.html"
+                    return False
+
+                for name in dirs:
+                    if nextSegment == name:
+                        pathToReturn += "/" + nextSegment
+                        proceed = True
+                        break
+
+                if proceed:
+                    continue
+                return False
+
+        return False
+
+    #removes "index.html" from a path if its there
+    def convertPathToURI(self, path):
+        pathSplits = path.split('/')
+        URI = path
+        if pathSplits.pop() == "index.html":
+            URI = "/".join(pathSplits)
+            URI += "/"
+
+        return URI
+
+    #returns the chars of a file given to a path
+    #assumes the path is valid
+    def getFile(self, path):
+        f = open("./www"+path, "r")
+        return f.read()
+    
+
     def handle(self):
         self.data = self.request.recv(1024).strip()
-        print ("Got a request of: %s\n" % self.data)
-        self.request.sendall(bytearray("OK",'utf-8'))
+
+        path = self.processRequest()
+        if not path:
+            self.response405()
+            return
+        
+        validatedPath = self.validateFilePath(path)
+        if not validatedPath:
+            self.response404()
+            return
+
+        URI = self.convertPathToURI(validatedPath)
+        if path == URI:
+            self.response200(validatedPath)
+        else:
+            self.response301(URI)
+
+        self.response404()
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
@@ -44,3 +195,5 @@ if __name__ == "__main__":
     # Activate the server; this will keep running until you
     # interrupt the program with Ctrl-C
     server.serve_forever()
+
+
